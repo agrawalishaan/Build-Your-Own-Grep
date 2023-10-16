f590f078f090da4876794538887065ebaf0a0b1f
agrawalishaan <113543798+agrawalishaan@users.noreply.github.com>
2023-10-15 23:36:49 -0400

    add support for matching a single digit

--- app/main.js
+++ app/main.js
@@ -1,5 +1,28 @@
+const PATTERN_DIGIT = "\\d";
+const ORDS_DIGIT_STR = [48, 57];
+
+function getCharCode(char) {
+  if (char.length !== 1) {
+    throw new Error(`Expected char to be length 1, got ${char}`);
+  }
+  return char.charCodeAt(0);
+}
+
 function matchPattern(inputLine, pattern) {
-  if (pattern.length === 1) {
+  console.log(`inputLine: ${inputLine}, pattern: ${pattern}`);
+
+  if (pattern === PATTERN_DIGIT) {
+    console.log("matching a digit");
+    for (const char of inputLine) {
+      const code = getCharCode(char);
+      console.log(`char code: ${code}`);
+      if (code >= ORDS_DIGIT_STR[0] && code <= ORDS_DIGIT_STR[1]) {
+        return true;
+      }
+    }
+    return false;
+  } else if (pattern.length === 1) {
+    console.log(`matching a char ${pattern}`);
     return inputLine.includes(pattern);
   } else {
     throw new Error(`Unhandled pattern ${pattern}`);
@@ -17,8 +40,10 @@ function main() {
   }
 
   if (matchPattern(inputLine, pattern)) {
+    console.log("match found");
     process.exit(0);
   } else {
+    console.log("no match found");
     process.exit(1);
   }
 }
